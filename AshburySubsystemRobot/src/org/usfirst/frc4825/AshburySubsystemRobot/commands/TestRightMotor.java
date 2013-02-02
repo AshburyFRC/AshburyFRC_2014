346bacb1b578ae98dc56815078068b54fff6e273
irisliu <iris.liu@ashbury.ca>
2013-02-02 08:10:52 -0800

    Put modified code from TestLeftMotor.java into TestRightMotor.java

--- AshburySubsystemRobot/src/org/usfirst/frc4825/AshburySubsystemRobot/commands/TestRightMotor.java
+++ AshburySubsystemRobot/src/org/usfirst/frc4825/AshburySubsystemRobot/commands/TestRightMotor.java
@@ -9,11 +9,15 @@
 // it from being updated in th future.
 package org.usfirst.frc4825.AshburySubsystemRobot.commands;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc4825.AshburySubsystemRobot.Robot;
+import org.usfirst.frc4825.AshburySubsystemRobot.Support.UserMessagePrinter;
 /**
  *
  */
 public class  TestRightMotor extends Command {
+    double speedToExecute = 0.0;
+    double timeToExecute = 0.0;
     public TestRightMotor() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
@@ -24,19 +28,30 @@ public class  TestRightMotor extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
+        timeToExecute = SmartDashboard.getNumber("Test Right Motor Timeout");
+	speedToExecute = SmartDashboard.getNumber("Test Right Motor Speed");
+	setTimeout(timeToExecute);
+	Robot.driveTrain.setMotorOutputs(speedToExecute, 0.0);
+        
+	System.out.println( "initialize test right motor for " + timeToExecute + " seconds at " + speedToExecute );
+	UserMessagePrinter.printUserMessageLine1("Running Test Right Motor Command");        
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.driveTrain.setMotorOutputs(speedToExecute, 0.0);
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.driveTrain.stop();
+        UserMessagePrinter.printUserMessageLine1("");
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
