3915bcde8b6ac8fc1f84d5455c2e2672007694f4
macy0807 <macy0807@msn.com>
2013-02-09 11:14:11 -0800

    added the instance field for storing all the values for the robot~

---
+++ AshburySubsystemRobot/src/org/usfirst/frc4825/AshburySubsystemRobot/Robot_InstanceField.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc4825.AshburySubsystemRobot;
+
+/**
+ *
+ * @author Macy
+ */
+
+public class Robot_InstanceField 
+{  
+    static double ConveyorBelt = 0.15;
+    public static double getBeltValue()
+    {
+        return ConveyorBelt;
+    }
+
+}
