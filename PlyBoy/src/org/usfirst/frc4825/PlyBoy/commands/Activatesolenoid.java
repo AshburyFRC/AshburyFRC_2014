da38fdc9e64788e75113aab1bf47287dd41b28e7
Omnifade <isidorehrlich12@gmail.com>
2014-02-01 12:27:55 -0800

    solenoid

--- PlyBoy/src/org/usfirst/frc4825/PlyBoy/commands/Activatesolenoid.java
+++ PlyBoy/src/org/usfirst/frc4825/PlyBoy/commands/Activatesolenoid.java
@@ -8,23 +8,19 @@ import org.usfirst.frc4825.PlyBoy.subsystems.SolenoidSystem;
 
 public class  Activatesolenoid extends Command {
     //Declaring Data
-    boolean solenoidCommand;//For use without calling the meathod repetitively
-    SolenoidSystem solenoidSystem;//So it can be called in a static context
+    boolean solenoidCommand = true;//For use without calling the meathod repetitively
+    SolenoidSystem solenoidSystem = Robot.solenoidSystem;//So it can be called in a static context
     
     public Activatesolenoid() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-        
+        requires(solenoidSystem);
     }
     // Called just before this Command runs the first time
     protected void initialize() {
-        //Being used like a constructor
-        solenoidCommand = true;
-        solenoidSystem = new SolenoidSystem();
+        
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        solenoidCommand = SmartDashboard.getBoolean("Solenoid Activation", false);//Getting boolean value from the smart dashboard
+        solenoidCommand = SmartDashboard.getBoolean("Solenoid Activation");//Getting boolean value from the smart dashboard
         //Determining the action to preform
         if(solenoidCommand == true){
             solenoidSystem.setSolenoidOn();
@@ -32,8 +28,6 @@ public class  Activatesolenoid extends Command {
         else{
             solenoidSystem.setSolenoidOff();
         }
-        //Putting the solenoid state on the smart dashboard
-        SmartDashboard.putBoolean("Cylinder Activated", solenoidSystem.getSolenoidValue());
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
