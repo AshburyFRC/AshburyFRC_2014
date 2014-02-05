5c9ad04420f17c1c1bc0906d8e122f53e4e41797
avalS117 <vyacheslav.petrunyak@gmail.com>
2014-02-04 16:11:46 -0800

    Added pneumatics end() in refuelPneumatics

--- FRC_2014/src/org/usfirst/frc4825/FRC_2014/commands/refuelPneumatics.java
+++ FRC_2014/src/org/usfirst/frc4825/FRC_2014/commands/refuelPneumatics.java
@@ -36,10 +36,11 @@ public class  refuelPneumatics extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
-        //System.out.println("End refuelPneumatics");
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        RobotMap.cmpr.stop();
+        System.out.println("End refuelPneumatics");
     }
 }
